e66901cdc31712bb421710334565c017acde534b
bmcho12 <bmcho12@nm-metaworld.com>
2025-04-18 11:11:13 +0900

    add: discovery-server 기본 설정

--- x-discovery-server/build.gradle.kts
+++ x-discovery-server/build.gradle.kts
@@ -5,4 +5,5 @@ apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 }
